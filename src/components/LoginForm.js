ce2c02cf960e83ae66bfc02a4f6d9cd7c5f678e6
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-02 13:53:28 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,38 +1,44 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
       username: "",
-      password: "",
+      password: ""
     };
-    
-    this.handleInputChange = this.handleInputChange.bind(this);
-    
   }
 
-  handleInputChange(event) {
+  //look for the value of name and change its state
+  handleInputChange = event => {
     this.setState({
       [event.target.name]: event.target.value
-    });
+    })
+  }
+
+  //username and password fields must not be empty in order to be saved
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.username && this.state.password) {
+      this.props.onSubmit
+    }
   }
 
   render() {
-    const{username, password} = this.state;
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" name="username" type="text" placeholder="Username" onChange={this.handleInputChange} required />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
